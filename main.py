ed605f37a767211365eafcee73615fdf0e85a122
poki-musi <poki.musi@gmail.com>
2022-11-21 18:28:25 +0100

    P6

--- main.py
+++ main.py
@@ -2,32 +2,44 @@ import os
 from parser import CParser, CLexer, ParserError
 from resolver import Resolver, ResolverError
 from compiler import Compiler, CompilerError
+from datetime import datetime
 
 
 def process_file(inp):
-    try:
-        tokens = CLexer().tokenize(inp)
-        ast = CParser().parse(tokens)
-        res = Resolver().resolve(ast)
-        return Compiler.of_resolver(res).compile(ast).generate()
+    tokens = CLexer().tokenize(inp)
+    ast = CParser().parse(tokens)
+    res = Resolver().resolve(ast)
+    return Compiler.of_resolver(res).compile(ast).generate()
 
-    except (ParserError, ResolverError, CompilerError) as e:
-        print(e)
-        return None  # TODO
+def run_tests(fh):
+    log = fh.write
+
+    for file in next(os.walk("examples/pass"))[2]:
+        with open(f"examples/pass/{file}", "r") as f:
+            try:
+                log("== " * 10 + "pass/" + file + " ==" * 10)
+                log(process_file(f.read()))
+            except (ParserError, ResolverError, CompilerError) as e:
+                log(str(e))
+
+    for file in next(os.walk("examples/error"))[2]:
+        with open(f"examples/error/{file}", "r") as f:
+            try:
+                log("== " * 10 + "error/" + file + " ==" * 10, file=fh)
+                log(process_file(f.read()))
+            except (ParserError, ResolverError, CompilerError) as e:
+                log(str(e))
 
 
 def main():
-    try: os.mkdir("./outputs")
-    except: pass
-    try: os.mkdir("./examples")
-    except: pass
-    for file in next(os.walk("examples/"))[2]:
-        with open(f"examples/{file}", "r") as f:
-            print("== " * 10 + file + " ==" * 10)
-            asm = process_file(f.read())
+    try:
+        os.mkdir("./out")
+    except:
+        pass
 
-        with open(f"outputs/{file}.asm", "w") as f:
-            f.write(asm)
+    with open(f"out/{datetime.now()}.log", "w") as fh:
+        print("here")
+        run_tests(fh)
 
 
 if __name__ == "__main__":
